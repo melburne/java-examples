206a15825768acba0609f10f6ed17417eb55d233
melburne <melburne42@gmail.com>
2021-02-26 23:38:48 +0530

    Add method to convert object to json string

--- src/customannotations/TestCustomAnnotations.java
+++ src/customannotations/TestCustomAnnotations.java
@@ -1,29 +1,43 @@
 package customannotations;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TestCustomAnnotations {
-  
+
   public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
     Person person = new Person("John", "Doe", "23");
-    checkIfSerializable(person);
-    initializeObject(person);
+    convertToJson(person);
   }
-  
+
+  private static void convertToJson(Object object) {
+    try {
+      checkIfSerializable(object);
+      initializeObject(object);
+      getJsonString(object);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
   private static void checkIfSerializable(Object object) {
-    if ( Objects.isNull(object)) {
+    if (Objects.isNull(object)) {
       throw new RuntimeException("Cannot serialize null object");
     }
-    
+
     Class<?> clazz = object.getClass();
     if (!clazz.isAnnotationPresent(JsonSerializable.class)) {
       throw new RuntimeException("Object is not annotated with JsonSerializable");
     }
   }
-  
-  private static void initializeObject(Object object) throws InvocationTargetException, IllegalAccessException {
+
+  private static void initializeObject(Object object)
+      throws InvocationTargetException, IllegalAccessException {
     Class<?> clazz = object.getClass();
     for (Method method : clazz.getDeclaredMethods()) {
       if (method.isAnnotationPresent(Init.class)) {
@@ -31,5 +45,24 @@ public class TestCustomAnnotations {
         method.invoke(object);
       }
     }
+  }
+
+  private static void getJsonString(Object object) throws IllegalAccessException {
+    Map<String, String> jsonMap = new HashMap<>();
+
+    Class<?> clazz = object.getClass();
+    for (Field field : clazz.getDeclaredFields()) {
+      field.setAccessible(true);
+      if (field.isAnnotationPresent(JsonElement.class)) {
+        jsonMap.put(field.getName(), (String) field.get(object));
+      }
+    }
+
+    String json =
+        jsonMap.entrySet().stream()
+            .map(e -> "\"" + e.getKey() + "\"" + ": \"" + e.getValue() + "\"")
+            .collect(Collectors.joining(", "));
+
+    System.out.println("{" + json + "}");
   }
 }
