4ecd2688ef5745f4d5977c5018cf5d1cbc3ec5f1
melburne <melburne42@gmail.com>
2021-02-27 22:06:00 +0530

    Add comments and JavaDocs for custom annotations

--- src/customannotations/Person.java
+++ src/customannotations/Person.java
@@ -1,19 +1,23 @@
 package customannotations;
 
+/**
+ * Sample class that can be serialized to a JSON String using {@link JsonSerializable}. {@link
+ * JsonElement} can be used on fields to include them in the JSON String and {@link Init} can be
+ * used to mark methods that must be run before the serialization.
+ */
 @JsonSerializable
 public class Person {
 
-  @JsonElement
-  private String firstName;
-  
-  @JsonElement
-  private String lastName;
-  
+  @JsonElement private String firstName;
+
+  @JsonElement private String lastName;
+
+  // use the 'personAge' key while serializing to JSON instead of 'age'
   @JsonElement(key = "personAge")
   private String age;
-  
+
   private String address;
-  
+
   public Person(String firstName, String lastName, String age, String address) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -21,6 +25,9 @@ public class Person {
     this.address = address;
   }
   
+  /**
+   * Converts the first letter of the 'firstName' and 'lastName' fields to upper case.
+   */
   @Init
   private void initNames() {
     this.firstName = this.firstName.substring(0, 1).toUpperCase() + this.firstName.substring(1);
--- src/customannotations/TestCustomAnnotations.java
+++ src/customannotations/TestCustomAnnotations.java
@@ -8,23 +8,26 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+/** Converts objects to JSON Strings if annotated with {@link JsonSerializable}. */
 public class TestCustomAnnotations {
 
-  public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+  public static void main(String[] args) {
     Person person = new Person("john", "doe", "23", "testAddress");
-    convertToJson(person);
-  }
 
-  private static void convertToJson(Object object) {
     try {
-      checkIfSerializable(object);
-      initializeObject(object);
-      getJsonString(object);
+      checkIfSerializable(person);
+      initializeObject(person);
+      getJsonString(person);
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
   }
 
+  /**
+   * Checks if the object is annotated with {@link JsonSerializable}.
+   *
+   * @param object Object to convert to JSON
+   */
   private static void checkIfSerializable(Object object) {
     if (Objects.isNull(object)) {
       throw new RuntimeException("Cannot serialize null object");
@@ -36,23 +39,36 @@ public class TestCustomAnnotations {
     }
   }
 
+  /**
+   * Invokes methods annotated with {@link Init}.
+   *
+   * @param object Object to convert to JSON
+   * @throws InvocationTargetException
+   * @throws IllegalAccessException
+   */
   private static void initializeObject(Object object)
       throws InvocationTargetException, IllegalAccessException {
     Class<?> clazz = object.getClass();
     for (Method method : clazz.getDeclaredMethods()) {
       if (method.isAnnotationPresent(Init.class)) {
-        method.setAccessible(true);
+        method.setAccessible(true);   // needed if method to be invoked is private
         method.invoke(object);
       }
     }
   }
 
+  /**
+   * Converts the object to a JSON String.
+   *
+   * @param object Object to convert to JSON
+   * @throws IllegalAccessException
+   */
   private static void getJsonString(Object object) throws IllegalAccessException {
     Map<String, String> jsonMap = new HashMap<>();
 
     Class<?> clazz = object.getClass();
     for (Field field : clazz.getDeclaredFields()) {
-      field.setAccessible(true);
+      field.setAccessible(true);    // needed if field is private
       if (field.isAnnotationPresent(JsonElement.class)) {
         jsonMap.put(getUserDefinedKeyMaybe(field), field.get(object).toString());
       }
@@ -65,7 +81,13 @@ public class TestCustomAnnotations {
 
     System.out.println("{" + json + "}");
   }
-  
+
+  /**
+   * Checks if a field has a custom key defined via {@link JsonElement}.
+   *
+   * @param field Field of the object to convert to JSON
+   * @return Custom key if present or field name otherwise
+   */
   private static String getUserDefinedKeyMaybe(Field field) {
     String key = field.getAnnotation(JsonElement.class).key();
     return key.isEmpty() ? field.getName() : key;
