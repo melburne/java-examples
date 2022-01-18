816a8a0bf6c581a6a09527ba855435aca3093ee6
melburne <melburne42@gmail.com>
2022-01-18 00:17:05 -0600

    Add primitive param to object

--- src/passingbyvalue/Customer.java
+++ src/passingbyvalue/Customer.java
@@ -2,9 +2,19 @@ package passingbyvalue;
 
 public class Customer {
   private String name;
+  private int id;
 
-  public Customer(String name) {
+  public Customer(String name, int id) {
     this.name = name;
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
--- src/passingbyvalue/PassingObjects.java
+++ src/passingbyvalue/PassingObjects.java
@@ -3,16 +3,25 @@ package passingbyvalue;
 // https://www.linkedin.com/learning/java-memory-management/how-objects-are-passed
 public class PassingObjects {
   public static void main(String[] args) {
-    Customer c = new Customer("Sally");
-    renameCustomer(c);
+    // example holds true even if c is declared final
+    Customer c = new Customer("Sally", 10);
 
+    renameCustomer(c);
     // prints Diane because renameCustomer() changed the object's pointer to the String on the heap
     System.out.println(c.getName());
+
+    changeId(c);
+    // same logic for primitives
+    System.out.println(c.getId());
   }
 
   private static void renameCustomer(Customer cust) {
     // the Customer object is NOT copied on the heap but a new copy of the variable that points to the
     // Customer object is created on the stack. Hence, the following line modifies the original object on the heap.
     cust.setName("Diane");
+  }
+
+  private static void changeId(Customer cust) {
+    cust.setId(23);
   }
 }
